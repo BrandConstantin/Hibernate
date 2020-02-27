d008dcd0a3ec7facbdc1613d5c86f47b034366e8
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-02-27 11:04:38 +0100

    create and delete object with one-to-one bidirectional relationship

---
+++ 01_OneToOne-bi/src/es/udemy/hibernate/objects/DeleteInstructorDetailDemo.java
@@ -0,0 +1,52 @@
+package es.udemy.hibernate.objects;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import es.udemy.hibernate.entity.Instructor;
+import es.udemy.hibernate.entity.InstructorDetail;
+import es.udemy.hibernate.entity.Student;
+
+public class DeleteInstructorDetailDemo {
+
+	public static void main(String[] args) {
+		// create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// start transaction
+			session.beginTransaction();
+			
+			// get the instructor detail object
+			int theId = 3;
+			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, theId);
+			
+			// print the instructor detail
+			System.out.println("-> " + tempInstructorDetail);
+			
+			// print the associetad instructor
+			System.out.println("The associeted instructor is " + tempInstructorDetail.getInstructor());
+			
+			// delete the instructor detail
+			System.out.println("Deleting " + tempInstructorDetail);
+			session.delete(tempInstructorDetail);
+			
+			//commit transaction
+			session.getTransaction().commit();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally{
+			session.close();
+			factory.close();
+		}
+	}
+
+}
