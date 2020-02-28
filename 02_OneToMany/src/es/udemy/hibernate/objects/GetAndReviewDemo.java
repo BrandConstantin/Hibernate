339785a18d13a40cd9e8f42fb483b1f73a99a73f
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-02-28 10:47:15 +0100

    get and delete courses and reviews, delete in cascade

---
+++ 02_OneToMany/src/es/udemy/hibernate/objects/GetAndReviewDemo.java
@@ -0,0 +1,48 @@
+package es.udemy.hibernate.objects;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import es.udemy.hibernate.entity.Course;
+import es.udemy.hibernate.entity.Instructor;
+import es.udemy.hibernate.entity.InstructorDetail;
+import es.udemy.hibernate.entity.Review;
+import es.udemy.hibernate.entity.Student;
+
+public class GetAndReviewDemo {
+
+	public static void main(String[] args) {
+		// create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.addAnnotatedClass(Review.class)
+				.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// start transaction
+			session.beginTransaction();
+			
+			// get the course
+			int theId = 10;
+			Course tempCourse = session.get(Course.class, theId);
+			
+			// print the course
+			System.out.println(tempCourse.getReviews());
+			// print course reviews
+			
+			//commit transaction
+			session.getTransaction().commit();
+		}finally{
+			session.close();
+			factory.close();
+		}
+	}
+
+}
