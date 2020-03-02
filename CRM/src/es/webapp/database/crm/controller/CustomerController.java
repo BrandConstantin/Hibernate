501883d58bdb38ac163531472daee0208b160337
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-03-02 23:48:30 +0100

    service layer

--- CRM/src/es/webapp/database/crm/controller/CustomerController.java
+++ CRM/src/es/webapp/database/crm/controller/CustomerController.java
@@ -5,22 +5,31 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import es.webapp.database.crm.dao.CustomerDAO;
 import es.webapp.database.crm.entity.Customer;
+import es.webapp.database.crm.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	/*
 	// need to inject the customer dao
 	@Autowired
 	private CustomerDAO customerDao; // ist the CustomerDAO interface name
-
-	@RequestMapping("/list")
+	 */
+	
+	// need to inject our customer service
+	@Autowired
+	private CustomerService customerService;
+	
+//	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
-		// get customers from the dao
-		List<Customer> theCustomers = customerDao.getCustomers();
+		// get customers from the service
+		List<Customer> theCustomers = customerService.getCustomers();
 		
 		// add the customers to the model
 		theModel.addAttribute("customers", theCustomers);
