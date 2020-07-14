9c9fdc1ce1cd5e0631081dc53e939b7e207eede6
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-07-14 19:30:58 +0200

    relaciones many to one curso hibernate

---
+++ ProyectoRelacionesHibernate/src/es/hibernate/conexion/Pedido.java
@@ -0,0 +1,64 @@
+package es.hibernate.conexion;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+public class Pedido {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="fecha")
+	private Date fecha;
+	
+	@Column(name="forma_pago")
+	private String formaPago;
+	
+	@ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+	@JoinColumn(name="cliente_id")
+	private Cliente cliente;
+
+	public Pedido(Date fecha) {
+		super();
+		this.fecha = fecha;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFormaPago() {
+		return formaPago;
+	}
+
+	public void setFormaPago(String formaPago) {
+		this.formaPago = formaPago;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public String toString() {
+		return "Pedido [id=" + id + ", fecha=" + fecha + ", formaPago=" + formaPago + "]";
+	}
+	
+		
+}
